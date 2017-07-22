63cfdaffbe1d49958654c354b80a4865f733fe54
Robert Burns <burnsra@gmail.com>
2017-07-21 22:31:43 -0400

    Initial Commit

---
+++ Silhouette/SilhouetteView.h
@@ -0,0 +1,13 @@
+//
+//  SilhouetteView.h
+//  Silhouette
+//
+//  Created by Burns, Robert A on 7/21/17.
+//  Copyright © 2017 Burns5. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SilhouetteView : ScreenSaverView
+
+@end
